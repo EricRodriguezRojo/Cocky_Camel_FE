47e7b1947c6f16fb1c7cb4b3e8043544d16e9f6c
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-12-17 15:41:31 +0100

    Nurses List Screen modified #19

--- app/src/main/java/com/example/cockycamel/ui/theme/NursesListScreen.kt
+++ app/src/main/java/com/example/cockycamel/ui/theme/NursesListScreen.kt
@@ -5,15 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,29 +16,19 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cockycamel.R
 
-data class Nurse(
-    val nombre: String,
-    val especialidad: String,
-    val experiencia: Int
-)
-
 @Composable
-fun NursesListScreen(onBack: () -> Unit) {
+fun NursesListScreen(viewModel: AppViewModel, onBack: () -> Unit) {
 
-    val enfermeros = listOf(
-        Nurse("María López", "Pediatría", 5),
-        Nurse("Juan Pérez", "Urgencias", 8),
-        Nurse("Ana Torres", "Cuidados intensivos", 6),
-        Nurse("David Soler", "Geriatría", 4),
-        Nurse("Lucía Martín", "Cardiología", 7)
-    )
+    val uiState by viewModel.uiState.collectAsState()
+    val enfermeros = uiState.enfermeros
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
             .padding(24.dp)
-    ) {
+        ) {
+
 
         Text(
             text = stringResource(R.string.nurses_list_title),
@@ -59,10 +42,11 @@ fun NursesListScreen(onBack: () -> Unit) {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            items(enfermeros) { enfermero ->
+            items(enfermeros) { enfermero -> // Usamos la lista del ViewModel
                 NurseCard(enfermero)
             }
         }
+
         Spacer(modifier = Modifier.height(20.dp))
 
         Button(
@@ -74,8 +58,6 @@ fun NursesListScreen(onBack: () -> Unit) {
         ) {
             Text(stringResource(R.string.btn_go_back))
         }
-
-        Spacer(modifier = Modifier.height(10.dp))
     }
 }
 
